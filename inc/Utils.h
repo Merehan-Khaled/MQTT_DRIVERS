1b7843b6fa46057bf620be5d5646172c53009845
Merehan-Khaled <126336389+Merehan-Khaled@users.noreply.github.com>
2023-02-25 23:04:25 +0200

    Add files via upload

---
+++ inc/Utils.h
@@ -0,0 +1,14 @@
+/*
+ * Utils.h
+ *
+ *  Created on: Feb 25, 2023
+ *      Author: ELSERAG FOR LAPTOP
+ */
+
+#ifndef INC_UTILS_H_
+#define INC_UTILS_H_
+
+u8 Utils_NumberToBin(u32 number, u8 arr[]);
+u8 Utils_NumberToHex(u32 number, u8 arr[]);
+
+#endif /* INC_UTILS_H_ */
